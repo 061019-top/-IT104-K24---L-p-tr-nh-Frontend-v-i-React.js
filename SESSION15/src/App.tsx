bc08210b3db22fe73c4e8becf9d94d5aa1749045
061019-top <chudinhminh21@gmail.com>
2025-08-25 19:35:32 +0700

    mih group

---
+++ SESSION15/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Ex2 from "./components/PTIT_CNTT3_IT104_SS15_Bai2/Ex2";
+
+function App() {
+  return (
+    <div>
+    <></>
+    </div>
+  );
+}
+
+export default App;
